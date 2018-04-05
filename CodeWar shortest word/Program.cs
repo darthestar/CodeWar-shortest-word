649a16e34de1371db174e03c784f1b1be086e99a
Sparks <dsparks@proforma.com>
2018-04-05 08:46:49 -0400

    first solution

--- CodeWar shortest word/Program.cs
+++ CodeWar shortest word/Program.cs
@@ -8,8 +8,29 @@ namespace CodeWar_shortest_word
 {
     class Program
     {
+        
+        static int FindShort(string s)
+        {
+            int shortestWord = 100;
+
+            string[] splitString = s.Split();
+            foreach (string word in splitString)
+            {
+                if (word.Length < shortestWord)
+                {
+                    shortestWord = word.Length;
+                }
+            }
+            return shortestWord;
+
+        }
+
         static void Main(string[] args)
         {
+
+            int result = FindShort("bitcoin take over the world maybe who knows perhaps");
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
